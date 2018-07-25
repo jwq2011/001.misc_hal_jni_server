a8cb77ba1458a93e314463e3220d1d4942c92539
jiaweiqing <jiaweiqing@roadrover.cn>
2018-07-25 13:32:53 +0800

    [driver] ioctl set/get success.

--- misc_driver/systemstatus.c
+++ misc_driver/systemstatus.c
@@ -41,7 +41,7 @@ unsigned char StartupTimes=0;
 
 static struct timer_list SystemStartupTimer;
 
-#define READ_FROM_MMC_REGISTER
+#define READ_FROM_MMC_REGISTER 1
 
 #define ADD_SAVE_BL_CONFIG 1
 #ifdef ADD_SAVE_BL_CONFIG
@@ -72,7 +72,9 @@ static DEFINE_MUTEX(nvram_mutex);
 unsigned char SystemBrightness = -1;
 unsigned char GetSystemBlFlag = 0;
 
-#ifdef READ_FROM_CPU_REGISTER
+#define READ_FROM_CPU_REGISTER 0
+
+#if  READ_FROM_CPU_REGISTER
 #define ANDROID_CONFIG_BOOT	(~((1 << 7) | (1 << 8)))
 static long get_backlight_config(void)
 {
@@ -160,13 +162,14 @@ bl_write (struct file *file, const char __user *buf, size_t ret, loff_t *ppos)
 #endif
 }
 
-#elif (define READ_FROM_MMC_REGISTER)
+#elif ( READ_FROM_MMC_REGISTER)
 static unsigned int get_boot_config(void)
 {
     struct file *fd;
 //    size_t ret;
     int ret;
-    unsigned char buff[513];
+    unsigned char buff[10];
+	unsigned int value;
 
     loff_t off;
 
@@ -174,10 +177,10 @@ static unsigned int get_boot_config(void)
 
     old_fs = get_fs();
     set_fs(KERNEL_DS);
-    fd = filp_open("/dev/block/mmcblk3p8", O_RDONLY, 0);
-    off = 0x5ffa00;
+    fd = filp_open("/dev/block/mmcblk3", O_RDONLY, 0);
+    off = 0x7FFE00;
 
-    ret = vfs_read(fd,buff, 7, &off);
+    ret = vfs_read(fd,buff, sizeof(buff), &off);
 	if (IS_ERR(ret)) {
 		printk(KERN_ERR "%s vfs_read fail!\n", __func__);
 		goto CLOSE_FILP;
@@ -191,24 +194,29 @@ static unsigned int get_boot_config(void)
 
 	if((buff[0]==0xaa) || (buff[1]==0x11))
 	{
-		GetSystemBlFlag = 1;
-		SystemBrightness = (char)(buff[2 ]- '0')*100 + (char)(buff[3] - '0')*10 + (char)(buff[4] - '0');
+//		GetSystemBlFlag = 1;
+//		SystemBrightness = (char)(buff[2]- '0')*1000 + (char)(buff[3] - '0')*100 + (char)(buff[4] - '0');
+//        sscanf();
 
 		printk("vfs_read BL%d %x %x %x %x %x..\n", SystemBrightness, buff[2], buff[3], buff[4]);
 	}
 
-	printk("vfs_read %x %x %x %x %x..\n",buff[2], buff[3], buff[4], buff[5], buff[6]);
+//	printk("vfs_read %s %s %s %s %s %s %s %s..\n",buff[2],buff[3],buff[4],buff[5],buff[6],buff[7],buff[8],buff[9]);
+    printk(KERN_ERR "vfs_read %s\n", buff+2);
 
 CLOSE_FILP:
 	filp_close(fd, NULL);
 
-	return SystemBrightness;
+//	return SystemBrightness;
+	sscanf(buff+2, "%x", &value);
+	printk(KERN_ERR "value = %d\n", value);
+    return value;
 }
 
 static int set_boot_config(unsigned char *buf,unsigned char len)
 {
     struct file *fd;
-    size_t ret;
+    int ret;
     unsigned char buff[513];
 
     loff_t off;
@@ -217,7 +225,7 @@ static int set_boot_config(unsigned char *buf,unsigned char len)
 
     old_fs = get_fs();
     set_fs(KERNEL_DS);
-    fd = filp_open("/dev/block/mmcblk3p8", O_RDWR, 0);
+    fd = filp_open("/dev/block/mmcblk3", O_RDWR, 0);
 	if (IS_ERR(fd)) {
 		printk(KERN_ERR "%s open fail!\n", __func__);
 		goto CLOSE_FILP;
@@ -225,7 +233,7 @@ static int set_boot_config(unsigned char *buf,unsigned char len)
 #ifdef OLD_CHANGE
     off = 0x3ffa00;	// 4MB - 0x600
 #else
-	off = 0x5ffa00;	// 6MB - 0x600
+	off = 0x7FFE00;	// 6MB - 0x600
 #endif
 
     buff[0]=0xaa;
@@ -243,12 +251,13 @@ static int set_boot_config(unsigned char *buf,unsigned char len)
 		goto CLOSE_FILP;
 	}
 
-    printk("vfs_write %x %x %x %x %x ..\n",buff[2],buff[3],buff[4]);
+//	printk(KERN_ERR "vfs_write %s %s %s %s %s %s %s %s ..\n",buff[2],buff[3],buff[4],buff[5],buff[6],buff[7],buff[8],buff[9]);
+	printk(KERN_ERR "vfs_write %s \n", &buff[2]);
 
 CLOSE_FILP:
 	filp_close(fd, NULL);
 
-	return ret;
+	return 0;
 }
 
 #if 0
@@ -377,6 +386,7 @@ bl_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
         }
 	copy_to_user(buf, tmp, 1);
 #else
+
 	struct file *fd;
     size_t ret;
     unsigned char buff[1024];
@@ -388,10 +398,9 @@ bl_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
     old_fs = get_fs();
     set_fs(KERNEL_DS);
     fd = filp_open("/dev/block/mmcblk3", O_RDONLY, 0);
-//    off = 0x5ffa00;
-    off = 0x17800000;
+    off = 0x7FFE00;
 
-    ret = vfs_read(fd,buff, 0xff, &off);
+    ret = vfs_read(fd,buff, 2+4, &off);
 	if (IS_ERR(ret)) {
 		printk(KERN_ERR "%s vfs_read fail!\n", __func__);
 		goto CLOSE_FILP;
@@ -403,60 +412,114 @@ bl_read(struct file *file, char __user *buf, size_t nbytes, loff_t *ppos)
 		goto CLOSE_FILP;
 	}
 
+//    get_boot_config();
+    unsigned char *mbuff;
+//    get_boot_config();
+    printk("vfs_read %x %x %x %x %x..\n",buff[2], buff[3], buff[4], buff[5]);
+
     int i = 0;
-    printk("vfs_write :");
-    for (i; i<1024; i++)
+    printk("vfs_read :");
+    for (i; i<6; i++)
         printk("%x ",buff[i]);
     printk("\n");
-
-CLOSE_FILP:
+    copy_to_user(buf, (void *)buff, 6);
 
 #endif
+CLOSE_FILP:
 
 	return nbytes;	
 }
 
 static ssize_t
-bl_write (struct file *file, const char __user *buf, size_t ret, loff_t *ppos)
+bl_write (struct file *file, const char __user *buf, size_t count, loff_t *ppos)
 {
-
 	char mBuffer[10];
 
-	memset(mBuffer, 0, 10);
-	copy_from_user(mBuffer, buf, 10);
+	memset(mBuffer, 0, sizeof(mBuffer));
+	copy_from_user(mBuffer, buf, count);
+    mBuffer[count] = '\0';
 	printk(KERN_ERR "%d %s: buf = %s\n", __LINE__, __func__, buf);
 	//	__get_user(mBuffer, (char *)arg);
-#if 0
+#ifdef OLD_CHANGE
 	if(memcmp(mBuffer,"BL", 2) == 0)
 		set_backlight_config(mBuffer + 2, 3);
 #else
 
-//	set_backlight_config(mBuffer);
+    struct file *fd;
+    int ret;
+//    unsigned char buff[513];
+
+    loff_t off;
+
+    mm_segment_t old_fs;
+
+    old_fs = get_fs();
+    set_fs(KERNEL_DS);
+    fd = filp_open("/dev/block/mmcblk3", O_RDWR, 0);
+	if (IS_ERR(fd)) {
+		printk(KERN_ERR "%s open fail!\n", __func__);
+		goto CLOSE_FILP;
+	}
+#ifdef OLD_CHANGE
+    off = 0x3ffa00;	// 4MB - 0x600
+#else
+	off = 0x7FFE00;	// 6MB - 0x600
+#endif
+
+    mBuffer[0]=0xaa;
+    mBuffer[1]=0x11;
+	memcpy(mBuffer+2, mBuffer, count);
+    ret = vfs_write(fd, mBuffer, 2+count, &off);
+	if (IS_ERR(ret)) {
+		printk(KERN_ERR "%s vfs_write fail!\n", __func__);
+		goto CLOSE_FILP;
+	}
+
+    ret = vfs_fsync(fd, 0);
+	if (IS_ERR(ret)) {
+		printk(KERN_ERR "%s vfs_fsync fail!\n", __func__);
+		goto CLOSE_FILP;
+	}
+
+    printk(KERN_ERR "vfs_write %x %x %x %x %x %x ..\n",mBuffer[0],mBuffer[1],mBuffer[2], mBuffer[3],mBuffer[4],mBuffer[5]);
+
+CLOSE_FILP:
+	filp_close(fd, NULL);
+
+//	set_boot_config(mBuffer, sizeof(mBuffer));
+    return 0;
 #endif
 }
-
 
 static long bl_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	int ioarg=1;
-	unsigned char mBuffer[32];
+	unsigned char mBuffer[8];
 	unsigned int value;
 	mutex_lock(&nvram_mutex);
 
-	printk("usb_ioctl cmd=%x..\n",cmd);
+	printk("usb_ioctl cmd=%u..\n",cmd);
     switch (cmd) 
 	{
 		case IOCTL_ANDROID_GET_BOOTCONFIG:
 //			value = get_backlight_config();
-//            value = get_boot_config();
-//			copy_to_user(value, arg, sizeof(value));
+//            mBuffer = get_boot_config();
+			value = get_boot_config();
+//          memcpy(mBuffer, get_boot_config(), sizeof(mBuffer));
+//          sscanf(mBuffer, "%d", &value);
+            printk(KERN_ERR "IOCTL_ANDROID_GET_BOOTCONFIG %d\n", value);
+			copy_to_user(arg, &value, sizeof(value));
 			break;
 		case IOCTL_ANDROID_SET_BOOTCONFIG:
 			memset(mBuffer,0, sizeof(mBuffer));
-			copy_from_user(mBuffer, arg, sizeof(mBuffer));
+			copy_from_user(&value, arg, sizeof(value));
+            
+			/* switch Dec to Char*/
+			sprintf(mBuffer, "%x", value);
+			printk(KERN_ERR "IOCTL_ANDROID_SET_BOOTCONFIG %s\n", mBuffer);
 
 //			set_backlight_config(mBuffer);
-//            set_boot_config(mBuffer, sizeof(mBuffer));
+            set_boot_config(mBuffer, sizeof(mBuffer));
 			break;
         case IOCTL_ANDROID_SET_BOOT_LIGHTS:
         case IOCTL_ANDROID_SET_BOOT_CAMERA:
