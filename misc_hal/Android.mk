02ded17ae2edbb61850388676109b5d87032ab61
jiawq <jiawq@qq.com>
2019-01-16 19:48:29 +0800

    [HAL]稳定版提交.

--- misc_hal/Android.mk
+++ misc_hal/Android.mk
@@ -1,23 +1,15 @@
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(BOARD_HAS_EMMC_BOOT_CONFIG),true)
+
 include $(CLEAR_VARS)
 
 LOCAL_PRELINK_MODULE := false
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils
-LOCAL_SRC_FILES := misc_hal.c
-LOCAL_MODULE := misc_hal.$(TARGET_BOARD_PLATFORM)
-LOCAL_MODULE_TAGS := eng  
+LOCAL_SRC_FILES := emmc_misc.c
+LOCAL_MODULE := emmc_misc.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := eng
 include $(BUILD_SHARED_LIBRARY)
-     
 
- 
-
-    
-  
-
-
-   
-
-  
-
-
+endif
